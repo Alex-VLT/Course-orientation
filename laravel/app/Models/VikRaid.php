3d52ebea647bdcd31d3bc9f86ade58bf9c78737a
Matheo Duhamel <Matheo.duhamel@etu.unicaen.fr>
2026-01-06 18:07:36 +0100

    (fix) vikraid fixing

--- laravel/app/Models/VikRaid.php
+++ laravel/app/Models/VikRaid.php
@@ -13,28 +13,9 @@ class VikRaid extends Model
     public $timestamps = false;
     use HasFactory;
     
-    // Nom de la table dans la BDD
+   
     
-    // Clé primaire personnalisée
-    protected $primaryKey = 'RAID_NUM';
-    
-    // Indique si la clé primaire est auto-incrémentée (Oui c'est un INT)
-    public $incrementing = true;
-    
-    // Type de la clé primaire
-    protected $keyType = 'int';
-    
-    // Désactiver les timestamps si les colonnes created_at/updated_at n'existent pas dans ton script SQL
-    public $timestamps = false;
-    
-    // Colonnes remplissables
-    protected $fillable = [
-        'CLU_NUM', 'INS_ID', 'RAID_NOM', 'RAID_DATE_DEBUT_INSCRI',
-        'RAID_DATE_FIN_INSCRI', 'RAID_DATE_DEBUT', 'RAID_DATE_FIN',
-        'RAID_CONTACT', 'RAID_LIEN_SITE_WEB', 'RAID_LATITUDE',
-        'RAID_LONGITUDE', 'RAID_ILLUSTRATION'
-    ];
-
+  
     protected $keyType = 'int';
 
     protected $fillable = [
