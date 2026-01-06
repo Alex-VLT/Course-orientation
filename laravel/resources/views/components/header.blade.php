cd120a8801cf6c3e08f9a786ec8dc965718b6cf4
Heuze Raphael <heuzraph@gmail.com>
2026-01-06 14:24:20 +0100

    (ADD) Register Login and Logout are now working
    As a guest you can press in the header login to connect yourself, on the login page you can click on the register button in the header to register and when connected you can press the logout button

--- laravel/resources/views/components/header.blade.php
+++ laravel/resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-[#7DC2A5]">
     <div class="px-2 sm:px-4 lg:px-6 py-1">
         <div class="flex items-center justify-between">
-            <!-- Logo cliquable -->
+            <!-- Logo -->
             <a href="###" {{-- ### placeholder for "{{ route('home') }}" --}} 
                class="flex items-center">
                 <img src="{{ asset('images/logoEmbuscade.png') }}" 
@@ -9,11 +9,44 @@
                      class="h-16 sm:h-10 md:h-24 lg:h-16 w-auto object-contain">
             </a>
             
-            <!-- Bouton Se Connecter -->
-            <a href="##" {{-- ### placeholder for "{{ route('login') }}" --}}
-               class="bg-[#A67C52] text-black font-semibold px-6 py-2 rounded-md hover:bg-[#8B623D] transition-colors duration-200 whitespace-nowrap text-sm md:text-base">
-                Se Connecter
-            </a>
+            @auth
+                <div class="flex items-center gap-3 md:gap-4">
+                    
+                    <a href="##" {{-- ### placeholder for "{{ route('dashboard') }}" --}}
+                       class="bg-[#A67C52] text-black font-semibold px-4 py-2 md:px-6 md:py-2 rounded-md hover:bg-[#8B623D] transition-colors duration-200 whitespace-nowrap text-sm md:text-base">
+                        Dashboard
+                    </a>
+                    
+                    <form action="{{route('logout')}}" method="POST">
+                        @csrf
+                        <button type="submit"
+                        class="cursor-pointer bg-[#A67C52] text-black font-semibold px-4 py-2 md:px-6 md:py-2 rounded-md hover:bg-[#8B623D] transition-colors duration-200 whitespace-nowrap text-sm md:text-base">
+                            Se Déconnecter
+                        </button>
+                    </form>
+                    
+                    <a href="##" {{-- ### placeholder for "{{ route('profile') }}" --}}
+                       class="bg-[#A67C52] text-black font-semibold px-4 py-2 md:px-6 md:py-2 rounded-md hover:bg-[#8B623D] transition-colors duration-200 whitespace-nowrap text-sm md:text-base">
+                        Profil
+                    </a>
+                </div>
+            @endauth
+            
+            @guest
+                @if(Route::is('login'))
+                    <a href="{{ route('register') }}"
+                    class="bg-[#A67C52] text-black font-semibold px-6 py-2 rounded-md hover:bg-[#8B623D] transition-colors duration-200 whitespace-nowrap text-sm md:text-base">
+                        S'inscrire
+                    </a>
+                    
+                @else
+                    <a href="{{ route('login') }}"
+                    class="bg-[#A67C52] text-black font-semibold px-6 py-2 rounded-md hover:bg-[#8B623D] transition-colors duration-200 whitespace-nowrap text-sm md:text-base">
+                        Se Connecter
+                    </a>
+                @endif
+                
+            @endguest
         </div> 
-    </div>
+    </div> 
 </nav>
