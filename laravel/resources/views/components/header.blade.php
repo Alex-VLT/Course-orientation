a6a0600fdb03439ef700e5e8ba86c93e4dbcccc6
Heuze Raphael <heuzraph@gmail.com>
2026-01-06 11:28:41 +0100

    (ADD) added the header
    still missing redirection on image clicked and button clicked

--- laravel/resources/views/components/header.blade.php
+++ laravel/resources/views/components/header.blade.php
@@ -1,2 +1,19 @@
-<nav>
+<nav class="bg-[#7DC2A5]">
+    <div class="px-2 sm:px-4 lg:px-6 py-1">
+        <div class="flex items-center justify-between">
+            <!-- Logo cliquable -->
+            <a href="###" {{-- ### placeholder for "{{ route('home') }}" --}} 
+               class="flex items-center">
+                <img src="{{ asset('images/logoEmbuscade.png') }}" 
+                     alt="L'EMBUSCADE" 
+                     class="h-16 sm:h-10 md:h-24 lg:h-16 w-auto object-contain">
+            </a>
+            
+            <!-- Bouton Se Connecter -->
+            <a href="##" {{-- ### placeholder for "{{ route('login') }}" --}}
+               class="bg-[#A67C52] text-black font-semibold px-6 py-2 rounded-md hover:bg-[#8B623D] transition-colors duration-200 whitespace-nowrap text-sm md:text-base">
+                Se Connecter
+            </a>
+        </div> 
+    </div>
 </nav>
