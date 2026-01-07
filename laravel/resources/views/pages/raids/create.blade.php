4b8b73e0c5271516e75d345e570b3e1812697f49
AtsukaDev <atsukarisme@gmail.com>
2026-01-07 13:31:33 +0100

    (FIX) Fix header position on raid creation page

--- laravel/resources/views/pages/raids/create.blade.php
+++ laravel/resources/views/pages/raids/create.blade.php
@@ -113,8 +113,20 @@
             </div>
         </form>
     </div>
-    @push('scripts')
-    <script>
+    
+@push('styles')
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+      crossorigin=""/>
+
+<style>
+    #map { width: 100%; height: 300px; }
+    #map .leaflet-tile, #map img.leaflet-tile { display:block; image-rendering:auto; }
+</style>
+@endpush
+
+@push('scripts')
+<script>
         (function(){
             const clubSelect = document.getElementById('CLU_NUM');
             const responsibleSelect = document.getElementById('INS_ID');
@@ -137,18 +149,6 @@
             document.addEventListener('DOMContentLoaded', filter);
         })();
     </script>
-@push('styles')
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-      crossorigin=""/>
-
-<style>
-    #map { width: 100%; height: 300px; }
-    #map .leaflet-tile, #map img.leaflet-tile { display:block; image-rendering:auto; }
-</style>
-@endpush
-
-@push('scripts')
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
         integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
         crossorigin=""></script>
