94e90568ee98ea63aa2176cbf4dfe01bf8ea8589
Mary Ilhan <ilhanmary427@gmail.com>
2026-01-08 08:43:54 +0100

    (fix) cursor for the button of logout

--- laravel/resources/views/components/header.blade.php
+++ laravel/resources/views/components/header.blade.php
@@ -56,7 +56,7 @@
                     <form action="{{route('logout')}}" method="POST" class="flex items-center">
                         @csrf
                         <button type="submit"
-                                class="flex items-center gap-2 bg-[#A67C52] text-black font-bold px-4 py-2.5 md:px-5 md:py-2.5 rounded-full shadow-sm hover:bg-[#8B623D] hover:shadow-md transform hover:-translate-y-0.5 transition-all duration-200">
+                                class="cursor-pointer flex items-center gap-2 bg-[#A67C52] text-black font-bold px-4 py-2.5 md:px-5 md:py-2.5 rounded-full shadow-sm hover:bg-[#8B623D] hover:shadow-md transform hover:-translate-y-0.5 transition-all duration-200">
                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/></svg>
                             <span class="hidden md:inline">Déconnexion</span>
                         </button>
