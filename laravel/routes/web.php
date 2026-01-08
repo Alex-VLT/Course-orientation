2f2baf3eda803e68c9c658990220186b5b009057
Mary Ilhan <ilhanmary427@gmail.com>
2026-01-08 16:30:26 +0100

    (FIX) Route race.unsubscribe

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -150,5 +150,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/api/users/search', [RaceController::class, 'searchUser'])->name('api.users.search');
     
     // If a user wants to leave:
-    Route::delete('/course/{cou_num}/me', [RaceController::class, 'unsubscribeParticipant'])->name('race.unsubscribe');
+    Route::delete('/course/{cou_num}/me', [RaceController::class, 'unsubscribeParticipant'])->name('race.team.unsubscribe');
+
+    // If a user wants to access to page of race
+    Route::get('/course/{cou_num}/me', [RaceController::class, 'show'])->name('race.unsubscribe');
 });
